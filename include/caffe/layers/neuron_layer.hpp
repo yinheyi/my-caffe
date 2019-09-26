578b226792e8d62b206492abdcabd761643b3e87
yinheyi <chinayinheyi@163.com>
2019-09-26 23:26:27 +0800

    注释完neuron_layer.cpp文件，它是激活函数层的接口类。

--- include/caffe/layers/neuron_layer.hpp
+++ include/caffe/layers/neuron_layer.hpp
@@ -9,21 +9,27 @@
 
 namespace caffe {
 
-/**
- * @brief An interface for layers that take one blob as input (@f$ x @f$)
- *        and produce one equally-sized blob as output (@f$ y @f$), where
- *        each element of the output depends only on the corresponding input
- *        element.
+/** 该类定义了所有的神经元层的接口，神经元层就是激活函数层了，例如sigmoid函数/tanh函数/relu函数等。
+    激活函数层的输入永远与输出的blob块的shape是相同的，一一对应嘛，只是通过激活函数对输入进行了映
+    射而已。
  */
 template <typename Dtype>
 class NeuronLayer : public Layer<Dtype> {
  public:
   explicit NeuronLayer(const LayerParameter& param)
      : Layer<Dtype>(param) {}
+
+  /** @brief 功能描述： 该函数的把top的shape设置为bottom相同的shape.
+      @param [in] bottom 神经元层的输入blob块
+      @param [in] top    神经元层的输出blob块
+   */
   virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
       const vector<Blob<Dtype>*>& top);
 
+  /** @brief 功能描述: 该函数返回神经元层输入需要的blob块的个数，它只需要一个blob块就可以了。 */
   virtual inline int ExactNumBottomBlobs() const { return 1; }
+
+  /** @brief 功能描述: 该函数返回神经元层输出的blob块的个数，它只输出一个blob块就可以了。*/
   virtual inline int ExactNumTopBlobs() const { return 1; }
 };
 
--- src/caffe/layers/neuron_layer.cpp
+++ src/caffe/layers/neuron_layer.cpp
@@ -4,12 +4,19 @@
 
 namespace caffe {
 
+// 该函数把top的shape设置为与bottom的shape相同。
 template <typename Dtype>
 void NeuronLayer<Dtype>::Reshape(const vector<Blob<Dtype>*>& bottom,
       const vector<Blob<Dtype>*>& top) {
   top[0]->ReshapeLike(*bottom[0]);
 }
 
+/* INSTANT_CLASS的宏定义如下所示：  使用类模板实例化了两个类。
+#define INSTANTIATE_CLASS(classname) \
+  char gInstantiationGuard##classname; \
+  template class classname<float>; \
+  template class classname<double>
+ */
 INSTANTIATE_CLASS(NeuronLayer);
 
 }  // namespace caffe
