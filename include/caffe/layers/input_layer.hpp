859e2d1eb546faf7a4bd8fe097be660c4d74d217
yinheyi <chinayinheyi@163.com>
2019-09-28 10:32:31 +0800

    读完 inputlayer文件。

--- include/caffe/layers/input_layer.hpp
+++ include/caffe/layers/input_layer.hpp
@@ -20,14 +20,24 @@ class InputLayer : public Layer<Dtype> {
  public:
   explicit InputLayer(const LayerParameter& param)
       : Layer<Dtype>(param) {}
+
+  /** @brief 功能描述： 该函数作的唯一工作就是把top块reshape成input_param参数
+      中指定的shape.
+      @param [in] bottom 没有使用到。
+      @param [in] top    存放数据的blob块。
+   */
   virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
       const vector<Blob<Dtype>*>& top);
+
   // Data layers have no bottoms, so reshaping is trivial.
   virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
       const vector<Blob<Dtype>*>& top) {}
 
   virtual inline const char* type() const { return "Input"; }
+
+  // input 层作为最开始的一层，不需要bottom blob吗? 那它的数据是从哪里来的？
   virtual inline int ExactNumBottomBlobs() const { return 0; }
+  // 如果有label的话，可能输入是2个blobs块吧。
   virtual inline int MinTopBlobs() const { return 1; }
 
  protected:
--- src/caffe/layers/input_layer.cpp
+++ src/caffe/layers/input_layer.cpp
@@ -4,6 +4,10 @@
 
 namespace caffe {
 
+/*  从layer_param_中可以得到input_param, 从innput_param中可以得到它设定的每一个top块的shape，
+    可能一个shape都没有，也可能只有一个shape(此时所有的top块都reshape成该shape), 也可能有
+    num_top(它top块的个数)个shape, 此时每一个shape与一个top块一一对应.
+ */
 template <typename Dtype>
 void InputLayer<Dtype>::LayerSetUp(const vector<Blob<Dtype>*>& bottom,
       const vector<Blob<Dtype>*>& top) {
