f1aef4a77ced76f6720d8988883c0a793f0196b5
yinheyi <chinayinheyi@163.com>
2019-10-10 23:14:57 +0800

    看完了softmax层

--- include/caffe/layers/softmax_layer.hpp
+++ include/caffe/layers/softmax_layer.hpp
@@ -10,7 +10,7 @@
 namespace caffe {
 
 /**
- * @brief Computes the softmax function.
+ * @brief 该类定义了softmax层，该层就是一个softmax函数。输入与输出的shape是相同的。
  *
  * TODO(dox): thorough documentation for Forward, Backward, and proto params.
  */
@@ -23,8 +23,8 @@ class SoftmaxLayer : public Layer<Dtype> {
       const vector<Blob<Dtype>*>& top);
 
   virtual inline const char* type() const { return "Softmax"; }
-  virtual inline int ExactNumBottomBlobs() const { return 1; }
-  virtual inline int ExactNumTopBlobs() const { return 1; }
+  virtual inline int ExactNumBottomBlobs() const { return 1; }    // bottom的输入只需要一个Blob就OK.
+  virtual inline int ExactNumTopBlobs() const { return 1; }       // top的输出也只需要一个blob就OK.
 
  protected:
   virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
@@ -36,11 +36,14 @@ class SoftmaxLayer : public Layer<Dtype> {
   virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
 
-  int outer_num_;
-  int inner_num_;
-  int softmax_axis_;
-  /// sum_multiplier is used to carry out sum using BLAS
+  int outer_num_;        // 这个值相当于输入了多少个图片。
+  int inner_num_;        // 这个值相当于把一个图片分别了好几块，每一块都进行softmax,在实际中，大多数下，inner_num_就是1而已，一个图片是一个类别.
+  int softmax_axis_;     // 它值表示在第几维度进行softmax操作，例如一个shape为[32,100, 5, 5]，而softmax_axis_为1,则表示在100的那个维度进行softmax,
+                         // 相当于共有100类别，此时out_num_的值为32, inner_num_的值为25.
+
+  // 定义这个一维向量(它的大小为shape[softmax_axis_]），目的是为了利用矩阵乘法求累加和,在forward和backward中会使用到。
   Blob<Dtype> sum_multiplier_;
+
   /// scale is an intermediate Blob to hold temporary results.
   Blob<Dtype> scale_;
 };
--- src/caffe/layers/softmax_layer.cpp
+++ src/caffe/layers/softmax_layer.cpp
@@ -9,33 +9,43 @@ namespace caffe {
 template <typename Dtype>
 void SoftmaxLayer<Dtype>::Reshape(const vector<Blob<Dtype>*>& bottom,
       const vector<Blob<Dtype>*>& top) {
-  softmax_axis_ =
-      bottom[0]->CanonicalAxisIndex(this->layer_param_.softmax_param().axis());
   top[0]->ReshapeLike(*bottom[0]);
+
+  // 在该维度上进行softmax操作, 该维度的值就相当于要分为多少类别。
+  softmax_axis_ = bottom[0]->CanonicalAxisIndex(this->layer_param_.softmax_param().axis());
   vector<int> mult_dims(1, bottom[0]->shape(softmax_axis_));
   sum_multiplier_.Reshape(mult_dims);
+  // 一维向量中的值全部设置为1, 目的就是使用矩阵相乘函数求累加和。
   Dtype* multiplier_data = sum_multiplier_.mutable_cpu_data();
   caffe_set(sum_multiplier_.count(), Dtype(1), multiplier_data);
+
   outer_num_ = bottom[0]->count(0, softmax_axis_);
   inner_num_ = bottom[0]->count(softmax_axis_ + 1);
+
+  // 初始化scale_的shape, 我发现在下面的代码中，scale_最多使用了inner_num个空间大小啊。
   vector<int> scale_dims = bottom[0]->shape();
   scale_dims[softmax_axis_] = 1;
   scale_.Reshape(scale_dims);
 }
 
+// 下面的函数代码，要知道sfotmax层干了什么事才行,要好好的推导一下。
 template <typename Dtype>
 void SoftmaxLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
     const vector<Blob<Dtype>*>& top) {
   const Dtype* bottom_data = bottom[0]->cpu_data();
   Dtype* top_data = top[0]->mutable_cpu_data();
   Dtype* scale_data = scale_.mutable_cpu_data();
-  int channels = bottom[0]->shape(softmax_axis_);
-  int dim = bottom[0]->count() / outer_num_;
+
+  int channels = bottom[0]->shape(softmax_axis_);  // 它的值就相当于分类的类别数。
+  int dim = bottom[0]->count() / outer_num_;       // 注意dim的大小。
+
   caffe_copy(bottom[0]->count(), bottom_data, top_data);
   // We need to subtract the max to avoid numerical issues, compute the exp,
   // and then normalize.
   for (int i = 0; i < outer_num_; ++i) {
-    // initialize scale_data to the first plane
+
+    // 该双层for循环求每一个channel的最大值， 最后求出来的scale_data可以看用是一个
+    // 1 * inner_num_的向量。
     caffe_copy(inner_num_, bottom_data + i * dim, scale_data);
     for (int j = 0; j < channels; j++) {
       for (int k = 0; k < inner_num_; k++) {
@@ -43,14 +53,18 @@ void SoftmaxLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
             bottom_data[i * dim + j * inner_num_ + k]);
       }
     }
+
     // subtraction
     caffe_cpu_gemm<Dtype>(CblasNoTrans, CblasNoTrans, channels, inner_num_,
         1, -1., sum_multiplier_.cpu_data(), scale_data, 1., top_data);
+
     // exponentiation
     caffe_exp<Dtype>(dim, top_data, top_data);
+
     // sum after exp
     caffe_cpu_gemv<Dtype>(CblasTrans, channels, inner_num_, 1.,
         top_data, sum_multiplier_.cpu_data(), 0., scale_data);
+
     // division
     for (int j = 0; j < channels; j++) {
       caffe_div(inner_num_, top_data, scale_data, top_data);
@@ -59,6 +73,7 @@ void SoftmaxLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
   }
 }
 
+// 下面求导数的代码， 你需要自己把公式推导一遍才行，具体不说了。
 template <typename Dtype>
 void SoftmaxLayer<Dtype>::Backward_cpu(const vector<Blob<Dtype>*>& top,
     const vector<bool>& propagate_down,
