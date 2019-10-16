4c16efbed5a0cb6bc525ce9855eca32ffe94aab6
yinheyi <chinayinheyi@163.com>
2019-10-16 23:57:57 +0800

    看完signal_handler.cpp文件。

--- src/caffe/util/signal_handler.cpp
+++ src/caffe/util/signal_handler.cpp
@@ -7,10 +7,18 @@
 #include "caffe/util/signal_handler.h"
 
 namespace {
+  /* 在信号处理函数中, 对static volatile sig_atomic_t类型的值进行操作，
+     然后直接返回, 这是标准的做法。 sig_atomic_t类型的变量保证了即使在
+     可能被信号处理程序中断时也保持原子性。 */
   static volatile sig_atomic_t got_sigint = false;
   static volatile sig_atomic_t got_sighup = false;
   static bool already_hooked_up = false;
 
+  /**
+    @brief SIGINT和SIGHUP信号的处理程序。 它的动作很简单，也是标准的做法：
+          对static volatile sig_atomic_t类型有变量进行操作变变直接返回。
+    @param [in] signal 信号的ID.
+   */
   void handle_signal(int signal) {
     switch (signal) {
     case SIGHUP:
@@ -22,6 +30,10 @@ namespace {
     }
   }
 
+  /**
+    @brief 该函数为信号SIGINT和信号SIGHUP绑定相应的信号处理函数。看这部分时，
+    要了解一下信号相应的知识，可以看一下unix核心编程一书。
+    */
   void HookupHandler() {
     if (already_hooked_up) {
       LOG(FATAL) << "Tried to hookup signal handlers more than once.";
@@ -45,6 +57,7 @@ namespace {
   }
 
   // Set the signal handlers to the default.
+  // 与上面函数相反，恢复默认的信号处理函数。
   void UnhookHandler() {
     if (already_hooked_up) {
       struct sigaction sa;
@@ -66,8 +79,10 @@ namespace {
     }
   }
 
-  // Return true iff a SIGINT has been received since the last time this
-  // function was called.
+  /* 每一次执行该函数时，不仅仅返回got_sigint的值，还会把它再置为false,意思就是
+     每当调用该函数时，实际上是在检测自从上次调用该函数到现在，got_sigint是否被
+     置为了true.
+   */
   bool GotSIGINT() {
     bool result = got_sigint;
     got_sigint = false;
@@ -89,13 +104,14 @@ SignalHandler::SignalHandler(SolverAction::Enum SIGINT_action,
                              SolverAction::Enum SIGHUP_action):
   SIGINT_action_(SIGINT_action),
   SIGHUP_action_(SIGHUP_action) {
-  HookupHandler();
+  HookupHandler();    // 绑定相应的信号处理函数。
 }
 
 SignalHandler::~SignalHandler() {
-  UnhookHandler();
+  UnhookHandler();  // 恢复至默认的信号处理函数。
 }
 
+/** @brief 根据相应的值，返回要执行的动作。 */
 SolverAction::Enum SignalHandler::CheckForSignals() const {
   if (GotSIGHUP()) {
     return SIGHUP_action_;
@@ -109,6 +125,7 @@ SolverAction::Enum SignalHandler::CheckForSignals() const {
 // Return the function that the solver can use to find out if a snapshot or
 // early exit is being requested.
 ActionCallback SignalHandler::GetActionFunction() {
+  // 这是使用了boost::bind函数，把一个类成员函数(它有一个this指针）包装成0个参数的函数对象。
   return boost::bind(&SignalHandler::CheckForSignals, this);
 }
 
