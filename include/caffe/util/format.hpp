4ec2d6424d8520c84a17b8c9054f3a954c0c8da0
殷和义 <1140195102@qq.com>
2019-10-20 11:42:49 +0800

    Update format.hpp
    增加了注释。

--- include/caffe/util/format.hpp
+++ include/caffe/util/format.hpp
@@ -7,6 +7,12 @@
 
 namespace caffe {
 
+ /**
+   @brief 功能描述：该函数实现把数int类型的数字转换为字符串。 并且不满足字符宽度的使用0来填充。
+   @param [in] n                      要转换的整数
+   @param [in] numberOfLeadingZeros   表示的显示宽度
+   @return 返回值是std::string类型, 即格式化之后的字符串。
+   */
 inline std::string format_int(int n, int numberOfLeadingZeros = 0 ) {
   std::ostringstream s;
   s << std::setw(numberOfLeadingZeros) << std::setfill('0') << n;
