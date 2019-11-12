2f7c71afbb8c27ea500ea2f8078fea947fe5bdd9
yinheyi <chinayinheyi@163.com>
2019-11-12 23:14:34 +0800

    搞定slcale_layer层的代码。

--- include/caffe/layers/scale_layer.hpp
+++ include/caffe/layers/scale_layer.hpp
@@ -33,31 +33,17 @@ class ScaleLayer: public Layer<Dtype> {
       const vector<Blob<Dtype>*>& top);
 
   virtual inline const char* type() const { return "Scale"; }
-  // Scale
+
+  /**
+    @brief 获取scale层的输入块的数目。 当输入为2时，bottom[1]用于存在scale的值;
+    如果输入为1时，scale的值作为本层的学习参数，它会根据梯度值进行更新的。
+    */
   virtual inline int MinBottomBlobs() const { return 1; }
   virtual inline int MaxBottomBlobs() const { return 2; }
+
   virtual inline int ExactNumTopBlobs() const { return 1; }
 
  protected:
-  /**
-   * In the below shape specifications, @f$ i @f$ denotes the value of the
-   * `axis` field given by `this->layer_param_.scale_param().axis()`, after
-   * canonicalization (i.e., conversion from negative to positive index,
-   * if applicable).
-   *
-   * @param bottom input Blob vector (length 2)
-   *   -# @f$ (d_0 \times ... \times
-   *           d_i \times ... \times d_j \times ... \times d_n) @f$
-   *      the first factor @f$ x @f$
-   *   -# @f$ (d_i \times ... \times d_j) @f$
-   *      the second factor @f$ y @f$
-   * @param top output Blob vector (length 1)
-   *   -# @f$ (d_0 \times ... \times
-   *           d_i \times ... \times d_j \times ... \times d_n) @f$
-   *      the product @f$ z = x y @f$ computed after "broadcasting" y.
-   *      Equivalent to tiling @f$ y @f$ to have the same shape as @f$ x @f$,
-   *      then computing the elementwise product.
-   */
   virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
       const vector<Blob<Dtype>*>& top);
   virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
@@ -67,14 +53,14 @@ class ScaleLayer: public Layer<Dtype> {
   virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
       const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
 
-  shared_ptr<Layer<Dtype> > bias_layer_;
-  vector<Blob<Dtype>*> bias_bottom_vec_;
-  vector<bool> bias_propagate_down_;
-  int bias_param_id_;
+  shared_ptr<Layer<Dtype> > bias_layer_;      // 当参数中选择了bias时，会在scale的基础上增加一个bias的值，通过bias_layer来完成。
+  vector<Blob<Dtype>*> bias_bottom_vec_;      // bias_layer层的输入
+  vector<bool> bias_propagate_down_;          // 表示bias_layer层中的bottom块是否需要梯度的反向传播。
+  int bias_param_id_;                         // bias层的偏置值在scale层中的bobs_中的第几个位置上。
 
-  Blob<Dtype> sum_multiplier_;
-  Blob<Dtype> sum_result_;
-  Blob<Dtype> temp_;
+  Blob<Dtype> sum_multiplier_;    // 一维向量，里面的值全为1,用于矩阵与向量的相乘实现对矩阵按行求和。
+  Blob<Dtype> sum_result_;        // 在求scale的梯度时使用，用于保存product在inner_dim维度上求和之后的结果
+  Blob<Dtype> temp_;              // 如果对输入进行原位操作时，作于保存原始的输入的值，在反向传播求梯度时需要。
   int axis_;
   int outer_dim_, scale_dim_, inner_dim_;
 };
--- src/caffe/layers/scale_layer.cpp
+++ src/caffe/layers/scale_layer.cpp
@@ -12,10 +12,17 @@ template <typename Dtype>
 void ScaleLayer<Dtype>::LayerSetUp(const vector<Blob<Dtype>*>& bottom,
       const vector<Blob<Dtype>*>& top) {
   const ScaleParameter& param = this->layer_param_.scale_param();
-  if (bottom.size() == 1 && this->blobs_.size() > 0) {
-    LOG(INFO) << "Skipping parameter initialization";
-  } else if (bottom.size() == 1) {
-    // scale is a learned parameter; initialize it
+
+  // 如果bottom块的数目为1，说明了没有指定的scale块的值(因为bottom[1]可以用于指定为scale的值),
+  // 那么,scale的值一定是作为了本层的参数了，它存放了blobs_块中。如果blobs_的size()>0，说明已经
+  // 初始化了scale的值。  但问题是：谁会初始化该值呢？？？
+  if (bottom.size() == 1 && this->blobs_.size() > 0)
+  {
+      LOG(INFO) << "Skipping parameter initialization";
+  }
+  else if (bottom.size() == 1)  // 此时说明没有初始化scale的值就需要对它进行初始化
+  {
+    // 首先从参数中获取axis_和num_aexs的值. 
     axis_ = bottom[0]->CanonicalAxisIndex(param.axis());
     const int num_axes = param.num_axes();
     CHECK_GE(num_axes, -1) << "num_axes must be non-negative, "
@@ -25,22 +32,27 @@ void ScaleLayer<Dtype>::LayerSetUp(const vector<Blob<Dtype>*>& bottom,
           << "scale blob's shape extends past bottom[0]'s shape when applied "
           << "starting with bottom[0] axis = " << axis_;
     }
+
+    // 接着，通过axis_和num_aexs的值来确定scale块的shape.
     this->blobs_.resize(1);
     const vector<int>::const_iterator& shape_start =
         bottom[0]->shape().begin() + axis_;
     const vector<int>::const_iterator& shape_end =
-        (num_axes == -1) ? bottom[0]->shape().end() : (shape_start + num_axes);
+        (num_axes == -1) ? bottom[0]->shape().end() : (shape_start + num_axes);  // 注意这里处理num_axes=-1的情况。
     vector<int> scale_shape(shape_start, shape_end);
     this->blobs_[0].reset(new Blob<Dtype>(scale_shape));
+
+    // 接着，对scale块进行初始化，如果没有指定初始化方法的话，会初始化为1.
     FillerParameter filler_param(param.filler());
     if (!param.has_filler()) {
-      // Default to unit (1) filler for identity operation.
       filler_param.set_type("constant");
       filler_param.set_value(1);
     }
     shared_ptr<Filler<Dtype> > filler(GetFiller<Dtype>(filler_param));
     filler->Fill(this->blobs_[0].get());
   }
+
+  // 如果在参数中选择了增加bias值，则创建一个bias层，用于完成 + bias 的操作。
   if (param.bias_term()) {
     LayerParameter layer_param(this->layer_param_);
     layer_param.set_type("Bias");
@@ -83,6 +95,9 @@ void ScaleLayer<Dtype>::Reshape(const vector<Blob<Dtype>*>& bottom,
   // with axis_ == 0 and (therefore) outer_dim_ == 1. (Setting axis_ to
   // bottom[0]->num_axes() - 1, giving inner_dim_ == 1, would be equally
   // performant.)
+
+  // 当num_axes的值为0时，表示scale值是一个标量，相当于所有的输入值都使用一个scale值进行缩放操作。
+  // 此时，把axis设置为0了，这样的接下来的计算很高效。
   axis_ = (scale->num_axes() == 0) ?
       0 : bottom[0]->CanonicalAxisIndex(param.axis());
   CHECK_GE(bottom[0]->num_axes(), axis_ + scale->num_axes())
@@ -101,9 +116,17 @@ void ScaleLayer<Dtype>::Reshape(const vector<Blob<Dtype>*>& bottom,
   } else {
     top[0]->ReshapeLike(*bottom[0]);
   }
+
+  // sum_result_向量值用于保存在inner_dim上求和之后的值, 求和之后就是一个大小就
+  // 等于outer_dim * scale_dim.
   sum_result_.Reshape(vector<int>(1, outer_dim_ * scale_dim_));
+  // sum_multiplier用于通过矩阵和向量的乘积实现对矩阵行求和，它里面的值全为1,至于
+  // 它的size()大小选择outer_dim和inner_dim中最大值的原因是在求scale的梯度时需要在
+  // inner_dim和outer_dim上对梯度的内积矩阵按行求和的。
   const int sum_mult_size = std::max(outer_dim_, inner_dim_);
   sum_multiplier_.Reshape(vector<int>(1, sum_mult_size));
+  // 这里有一点不明白，为什么只需要判断最后一个值是不是等于1就决定了整个sum_multiplier的
+  // 值是不是1呢？
   if (sum_multiplier_.cpu_data()[sum_mult_size - 1] != Dtype(1)) {
     caffe_set(sum_mult_size, Dtype(1), sum_multiplier_.mutable_cpu_data());
   }
@@ -113,20 +136,17 @@ void ScaleLayer<Dtype>::Reshape(const vector<Blob<Dtype>*>& bottom,
   }
 }
 
+// 正向传播。
 template <typename Dtype>
 void ScaleLayer<Dtype>::Forward_cpu(
     const vector<Blob<Dtype>*>& bottom, const vector<Blob<Dtype>*>& top) {
   const Dtype* bottom_data = bottom[0]->cpu_data();
+  // 如果是原位计算的话，这里把输入的值在被重写之前保存一份，在梯度的反向传播时会使用到。
   if (bottom[0] == top[0]) {
-    // In-place computation; need to store bottom data before overwriting it.
-    // Note that this is only necessary for Backward; we could skip this if not
-    // doing Backward, but Caffe currently provides no way of knowing whether
-    // we'll need to do Backward at the time of the Forward call.
     caffe_copy(bottom[0]->count(), bottom[0]->cpu_data(),
                temp_.mutable_cpu_data());
   }
-  const Dtype* scale_data =
-      ((bottom.size() > 1) ? bottom[1] : this->blobs_[0].get())->cpu_data();
+  const Dtype* scale_data = ((bottom.size() > 1) ? bottom[1] : this->blobs_[0].get())->cpu_data();
   Dtype* top_data = top[0]->mutable_cpu_data();
   for (int n = 0; n < outer_dim_; ++n) {
     for (int d = 0; d < scale_dim_; ++d) {
@@ -141,6 +161,7 @@ void ScaleLayer<Dtype>::Forward_cpu(
   }
 }
 
+// 反向传播
 template <typename Dtype>
 void ScaleLayer<Dtype>::Backward_cpu(const vector<Blob<Dtype>*>& top,
     const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom) {
@@ -150,16 +171,25 @@ void ScaleLayer<Dtype>::Backward_cpu(const vector<Blob<Dtype>*>& top,
   }
   const bool scale_param = (bottom.size() == 1);
   Blob<Dtype>* scale = scale_param ? this->blobs_[0].get() : bottom[1];
+
+  // 求scale值的梯度
   if ((!scale_param && propagate_down[1]) ||
       (scale_param && this->param_propagate_down_[0])) {
     const Dtype* top_diff = top[0]->cpu_diff();
     const bool in_place = (bottom[0] == top[0]);
     const Dtype* bottom_data = (in_place ? &temp_ : bottom[0])->cpu_data();
+
+    // 下面这里其实没有必要搞这么复杂的，真的很影响代码的可读性。下面的代码为了效率的考虑，
+    // 处理了特殊的场景： 
+    // 1. 每一个输入都有一个单独scale值的情况，此时scale->count() == bottom[0]->count().
+    // 2. inner_dim等于1的场景
+    // 3. outer_dim等于1或者outer_dim * sclale_dim =1的场景
     // Hack: store big eltwise product in bottom[0] diff, except in the special
     // case where this layer itself does the eltwise product, in which case we
     // can store it directly in the scale diff, and we're done.
     // If we're computing in-place (and not doing eltwise computation), this
     // hack doesn't work and we store the product in temp_.
+
     const bool is_eltwise = (bottom[0]->count() == scale->count());
     Dtype* product = (is_eltwise ? scale->mutable_cpu_diff() :
         (in_place ? temp_.mutable_cpu_data() : bottom[0]->mutable_cpu_diff()));
@@ -184,6 +214,7 @@ void ScaleLayer<Dtype>::Backward_cpu(const vector<Blob<Dtype>*>& top,
         caffe_cpu_gemv(CblasNoTrans, sum_result_.count(), inner_dim_,
                        Dtype(1), product, sum_mult, Dtype(0), sum_result);
       }
+
       if (outer_dim_ != 1) {
         const Dtype* sum_mult = sum_multiplier_.cpu_data();
         Dtype* scale_diff = scale->mutable_cpu_diff();
@@ -202,6 +233,8 @@ void ScaleLayer<Dtype>::Backward_cpu(const vector<Blob<Dtype>*>& top,
       }
     }
   }
+
+  // 求bottom[0]的梯度
   if (propagate_down[0]) {
     const Dtype* top_diff = top[0]->cpu_diff();
     const Dtype* scale_data = scale->cpu_data();
